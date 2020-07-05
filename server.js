4991da2fa1eecefb78d31b0e5dab40257ceb2167
hl05 <harshit@hipbar.com>
2020-07-06 00:55:06 +0530

    with es6 for jest, what a nightmare

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
+const pino = require("pino-http");
 
 const app = express();
 
+app.use(pino());
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.listen(8080, () => console.log(`Listening on port 8080`));
--- src/add.test.js
+++ src/add.test.js
@@ -1,4 +1,4 @@
-const add = require("./Sum")
+import { add } from "./Sum";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
