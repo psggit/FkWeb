89bfb1715c8656c695e63e8a574b400b0c2abc4d
hl05 <harshit@hipbar.com>
2020-07-06 01:02:05 +0530

    with a json logger

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const path = require("path");
-const pino = require("pino-http");
+const pinoHttp = require("pino-http");
+const pino = require("pino");
+
+//Setup logger
+const httpLogger = pinoHttp();
+const logger = pino();
 
 const app = express();
 
-app.use(pino());
+app.use(httpLogger);
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.listen(8080, () => console.log(`Listening on port 8080`));
+app.listen(8080, () => {
+  logger.info({ port: 8080 }, "express started");
+});
