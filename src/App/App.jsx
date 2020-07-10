c32cb707bd2ab574982529481b015be264730752
syde <syed@hipbar.com>
2020-07-11 00:45:51 +0530

    Added Files

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,24 +4,26 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./home/Home";
 import Cart from "./cart";
 import Search from "./search";
-import Navigation from "./navigation";
+import Navigation from "./common/navigation";
 
 function App() {
   return (
-    <Router>
-      <Navigation />
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/cart">
-          <Cart />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+     <Router>
+       <Switch>
+         <Route path="/">
+           <Home />
+         </Route>
+         <Route path="/search">
+           <Search />
+         </Route>
+         <Route path="/cart">
+           <Cart />
+         </Route>
+       </Switch>
+       <Navigation/>
+     </Router>
+    </div>
   );
 }
 
