458de81e23db5ab6779e1ae8297a8750357fbc9c
hl05 <harshit@hipbar.com>
2020-07-12 18:40:40 +0530

    move store to a seperate directory

--- src/index.js
+++ src/index.js
@@ -2,27 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import { homeReducer } from "./App/home";
-import { combineReducers } from "redux";
-import thunk from "redux-thunk";
 
 import * as Sentry from "@sentry/react";
 
 import { App } from "./App";
+import store from "./store";
+
 import "./styles.scss";
 
 Sentry.init({
   dsn: "https://5b4b4508b0ec4e9ba02ef4e0d3cb381b@sty.hipbar.com/10",
   environment: ARGS_SENTRY_ENV,
   release: ARGS_SENTRY_RELEASE,
-});
-
-const store = configureStore({
-  reducer: combineReducers({
-    home: homeReducer,
-  }),
-  middleware: [thunk],
 });
 
 var mountNode = document.getElementById("app");
