ad600f73a5f45d1678cc94e817767bf2fb3027df
hl05 <harshit@hipbar.com>
2020-07-12 19:06:14 +0530

    with a few more pieces

--- src/App/home/carousel/duck/operations.js
+++ src/App/home/carousel/duck/operations.js
@@ -1,13 +1,19 @@
 import { getHomeCarousel } from "./actions";
+import config from "../../../../config";
+
+const fetchAds = () => {
+  const URL =
+    "https://gremlin." +
+    config.BASE_DOMAIN +
+    "/consumer/marketing/ads/get_ads/15";
+  return fetch(URL, {
+    headers: { Authorization: "Bearer cb04d1e946864660a022c5c88d486fd0" },
+  });
+};
 
 const fetchHomeCarousel = () => {
   return (dispatch) => {
-    return fetch(
-      "https://gremlin.hipbar-dev.com/consumer/marketing/ads/get_ads/15",
-      {
-        headers: { Authorization: "Bearer cb04d1e946864660a022c5c88d486fd0" },
-      }
-    )
+    return fetchAds()
       .then((response) => response.json())
       .then((json) => {
         dispatch(getHomeCarousel(json));
