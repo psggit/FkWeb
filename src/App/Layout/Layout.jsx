c2311f315c5888ba4f96bed9a0454886b0bfea22
senthil1989 <senthil.tony@gmail.com>
2020-07-17 10:54:12 +0530

    layout and searchbox designed

---
+++ src/App/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Layout({children}) {
+
+  return (
+    <>
+<div className="layout-container">
+    {children}
+</div>
+    </>
+  );
+}
+
+export default Layout;
--- src/App/search/Search.jsx
+++ src/App/search/Search.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SearchBox from "./SearchBox";
+import Layout from "../Layout/Layout";
 
 function Search() {
-  return <div> Search </div>;
+  const [cancelBtn, SetCancelBtn] = useState(false);
+  const cancelEnable = (val) => {
+    SetCancelBtn(val);
+  };
+  useEffect(() => {
+    window.addEventListener("focusout", () => {
+      SetCancelBtn(false);
+    });
+  });
+  return (
+    <>
+      <div>Search</div>
+      <div className="search-container">
+        <SearchBox cancelEnable={cancelEnable} />
+        {cancelBtn ? <button>Cancel</button> : ""}
+      </div>
+      <Layout>
+      <div className="search-child">
+    <h6>What are you looking for today?</h6>
+    <p>We'll tell you where <br/>
+it's available!</p>
+    </div>
+      </Layout>
+    </>
+  );
 }
 
 export default Search;
