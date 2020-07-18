fd5ae7ceb6f8a3096d01c8510267eae7af12f3f6
syde <syed@hipbar.com>
2020-07-18 20:55:09 +0530

    Done

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,8 @@ import { combineReducers } from "redux";
 
 import { configureStore } from "@reduxjs/toolkit";
 import { homeReducer } from "../App/home";
-import { tcReducer } from "../App/agreeAndContinue"
-import { userInfoCreateReducer } from "../App/userBasicInfo"
+import { tcReducer } from "../App/agreeAndContinue";
+import { userInfoCreateReducer } from "../App/userBasicInfo";
 import { cartReducer } from "../App/common/cart";
 import thunk from "redux-thunk";
 
