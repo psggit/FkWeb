e8f873c71d0c73534ca5ef0bca5528b3c50ee680
syde <syed@hipbar.com>
2020-07-28 03:13:22 +0530

    Done

--- src/App/address/SelectAddress/selectAddressComponent.jsx
+++ src/App/address/SelectAddress/selectAddressComponent.jsx
@@ -27,10 +27,6 @@ function SelectAddressComponent(props) {
     <>
       <ToolbarComponent helpVisibility="true" title="Choose Address" />
       <AddressComponent {...props} />
-      <div className="add-new-address">
-        <div className="title">Add New Address</div>
-        <img src={AddAddressIcon} className="add-image" />
-      </div>
       <div className="page-container">
         <AddressComponent {...props} />
         <div className="add-new-address" onClick={showAddAddress}>
