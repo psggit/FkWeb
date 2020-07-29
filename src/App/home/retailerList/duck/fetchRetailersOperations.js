c8cd20f5fd426a052e4a2f1df994cd54a55283bd
syde <syed@hipbar.com>
2020-07-30 04:34:18 +0530

    Added Fetch Retailer Operations

---
+++ src/App/home/retailerList/duck/fetchRetailersOperations.js
@@ -0,0 +1,58 @@
+import {
+  fetchRetailersSuccessfull,
+  fetchRetailersFailure,
+  fetchRetailersInProgress,
+} from "./actions";
+import { fetchRetailersAPI } from "../../../../utils";
+
+const reqBodyFromState = (selectedAddress) => {
+  return JSON.stringify({
+    city_id: selectedAddress.city.id,
+    state_id: selectedAddress.state.id,
+    gps: selectedAddress.gps,
+    offset:0,
+    limit: 30
+  });
+};
+
+const processResponse = (dispatch) => {
+  return (res) => {
+    if (res.ok) {
+      dispatch(fetchRetailersSuccessfull());
+      return res.json();
+    }
+    if (res.status === 400) {
+      //TODO:@hl05 setup sentry here?
+      throw new Error("invalid params");
+    } else {
+      throw new Error("Something went wrong, try again");
+    }
+  };
+};
+
+const onSuccess = (dispatch) => {
+  return (data) => {
+    dispatch(fetchRetailersSuccessfull(data));
+  };
+};
+
+const onError = (dispatch) => {
+  return (err) => {
+    dispatch(fetchRetailersFailure(err));
+  };
+};
+
+const fetchRetailersOperation = (selectedAddress) => {
+  let reqBody = reqBodyFromState(selectedAddress);
+  return (dispatch) => {
+    dispatch(fetchRetailersInProgress());
+    fetchRetailersAPI(
+      reqBody,
+      processResponse(dispatch),
+      onSuccess(dispatch),
+      onError(dispatch)
+    );
+  };
+};
+
+export { fetchRetailersOperation };
