54b9a8102278ac6955b5f9a7ddb00c7e1eb35ce9
Lokesh <lokesh@hipbar.com>
2020-08-12 02:41:13 +0530

    update: search next btn

--- src/App/search/SearchComponent.jsx
+++ src/App/search/SearchComponent.jsx
@@ -71,7 +71,7 @@ function SearchComponent(props) {
     });
     if (totalCartItems > 0) {
       return (
-        <BottomNextComponent redirectPath="/cart" title="View Cart">
+        <BottomNextComponent redirectPath="/cart" isNav={true} title="View Cart">
           <CartContentComponent
             content={totalCartItems + " ITEMS | ₹ " + total}
           />
