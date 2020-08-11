64ca29d68fd47ad9c82da21c12c8d6a591696126
hl05 <harshit@hipbar.com>
2020-08-11 22:05:09 +0530

    add selectedState and selectedCity to local storage

---
+++ src/App/stateCity/duck/locationLocalStorageHandler.js
@@ -0,0 +1,10 @@
+import { selectCity, selectState } from "./actions";
+
+export const locationLocalStorageHandler = (state, action) => {
+  if (action.type === selectState.type) {
+    localStorage.setItem("selectState", JSON.stringify(action.payload));
+  }
+  if (action.type === selectCity) {
+    localStorage.setItem("selectCity", JSON.stringify(action.payload));
+  }
+};
