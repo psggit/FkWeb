23298e98e4c9b6b4c277b0ebb05f4661566a0e5f
hl05 <harshit@hipbar.com>
2020-08-12 20:04:54 +0530

    juspay scripts configured based on build environment

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,13 @@
+let JusPayScript = "https://sandbox.juspay.in/pay-v3.js";
+if (ARGS_BUILD_ENV == "production") {
+  JusPayScript = "https://api.juspay.in/pay-v3.js";
+}
+
 const CONFIG = {
   BASE_DOMAIN: ARGS_BASE_DOMAIN,
   GMAPS_KEY: "AIzaSyCHGLLAB117OiC9rDD9ON3gRP1LQLAAQmI",
   SCHEME: "https://",
+  JusPayScript: JusPayScript,
 };
 
 export default CONFIG;
