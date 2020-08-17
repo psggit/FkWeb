f69e2b6f55ae7e1ae4b37926894a59a578528a89
Lokesh <lokesh@hipbar.com>
2020-08-17 18:56:57 +0530

    update: check platform

--- src/App/agreeAndContinue/agreeAndContinueComponent.jsx
+++ src/App/agreeAndContinue/agreeAndContinueComponent.jsx
@@ -1,8 +1,17 @@
+import FKPlatform from "fk-platform-sdk/web";
 import "./styles/style.scss";
 import { Redirect } from "react-router-dom";
 import React from "react";
 import PropTypes from "prop-types";
 import { TcComponent, VisitHipBarComponent } from "./components";
+
+let fkPlatformActive = true;
+
+try {
+  new FKPlatform("hipbar");
+} catch (e) {
+  fkPlatformActive = false;
+}
 
 AgreeAndContinueComponent.propTypes = {
   showTC: PropTypes.bool,
@@ -12,16 +21,17 @@ AgreeAndContinueComponent.propTypes = {
 function AgreeAndContinueComponent(props) {
   const showTC = props.showTC;
 
-  //  if (!navigator.appVersion.includes("flipkart")) {
-  //    return <VisitHipBarComponent />;
-  //  }
-
-  if (showTC) {
-    return <TcComponent {...props} />;
-  } else if (props.selectedAddress !== null) {
-    return <Redirect to="/user/login" />;
+  if (fkPlatformActive) {
+    if (FKPlatform.isPlatformAvailable()) {
+      if (showTC) {
+        return <TcComponent {...props} />;
+      } else if (props.selectedAddress !== null) {
+        return <Redirect to="/user/login" />;
+      }
+      return <Redirect to="/statecity/select" />;
+    }
   }
-  return <Redirect to="/statecity/select" />;
+  return <VisitHipBarComponent />;
 }
 
 export { AgreeAndContinueComponent };
