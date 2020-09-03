cc00fead580870245b8c3e882961e2271ffc0e2f
Lokesh <lokesh@hipbar.com>
2020-09-03 19:04:40 +0530

    update: upi ui

--- src/App/payment/components/UPIVerifyComponent.jsx
+++ src/App/payment/components/UPIVerifyComponent.jsx
@@ -9,41 +9,57 @@ UPIVerifyComponent.propTypes = {
 };
 
 function UPIVerifyComponent(props) {
+  const paymentOptionsDetails = props.payment.paymentOptionsDetails;
+
   useEffect(() => {
     //  return () => {
     props.resetUPI();
     //};
   }, []);
 
+  function formatToConsistency(number) {
+    return String(number).padStart(2, "0");
+  }
+
   const children = ({ remainingTime }) => {
     const minutes = Math.floor(remainingTime / 60);
     const seconds = remainingTime % 60;
-    return `${minutes}:${seconds}`;
+    return `${formatToConsistency(minutes)}:${formatToConsistency(seconds)}`;
   };
 
   function TimerComponent() {
     return (
-      <CountdownCircleTimer
-        onComplete={() => {}}
-        isPlaying
-        duration={100}
-        colors="#e97c07"
-      >
-        {children}
-      </CountdownCircleTimer>
+      <div className="timer">
+        <CountdownCircleTimer
+          onComplete={() => {
+            console.log("Timer");
+          }}
+          isPlaying
+          size={140}
+          strokeWidth={8}
+          strokeLinecap="square"
+          rotation="clockwise"
+          duration={paymentOptionsDetails.upi_time_limit}
+          colors="#e97c07"
+        >
+          {children}
+        </CountdownCircleTimer>
+      </div>
     );
   }
 
   return (
     <>
       <ToolbarComponent helpVisibility={false} title="Complete Payment" />
-      <div className="page-container">
-        <div> 1. Open the UPI-linked mobile app</div>
-        <div> 2. Check pending transactions</div>
-        <div> 3. Complete payment </div>
+      <div className="page-container upi-verify-container">
+        <div className="msg"> 1. Open the UPI-linked mobile app</div>
+        <div className="msg"> 2. Check pending transactions</div>
+        <div className="msg"> 3. Complete payment </div>
         <TimerComponent />
-        <div>Please complete payment before the timer runs out</div>
-        <div>
+        <div className="note">
+          Please complete payment before the timer runs out
+        </div>
+        <div className="note">
           Please don&apos;t hit back button until the transaction is complete
         </div>
         <div className="button-container"></div>
